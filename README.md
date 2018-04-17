3e3ae1803e63c55ad00aa22e3d41f1de9f32f661
Mark-André Hopf <mhopf@mark13.org>
2018-04-17 01:06:34 +0100

    preparations initial push to github

---
+++ README.md
@@ -0,0 +1,8 @@
+# glue.js
+Welcome to the glue.js, an Object Request Broker (ORB) and Interface Definition Language (IDL) compiler
+
+This library is currently being written to develop <a href="https://github.com/markandre13/workflow">workflow - A collaborative real-time white- and kanban board</a>.
+
+The IDL takes some inspiration from the CORBA IDL.
+
+The protocol takes some inspiration from JSON-RPC.
--- orb.ts
+++ orb.ts
@@ -1,29 +1,29 @@
+/*
+ *  glue.js Object Request Broker (ORB) and Interface Definition Language (IDL) compiler
+ *  Copyright (C) 2018 Mark-André Hopf <mhopf@mark13.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import * as ws from "ws"
 const WebSocket = ws
 
-function connect(url: string): Promise<WebSocket> {
-    return new Promise<WebSocket>(function(resolve, reject) {
-        var server = new WebSocket(url)
-        server.onopen = function() {
-            console.log("promise: open")
-            resolve(server)
-        }
-        server.onmessage = function(message) {
-            console.log("promise: message")
-            resolve(message)
-        }
-        server.onerror = function(err) {
-            reject(err)
-        }
-    })
-}
-
 class ORB {
-    client?: ws
+    socket?: ws
     id: number
-    reqid: number
-    
     obj: Map<number, Stub>
+    reqid: number
     
     constructor() {
         this.id = 0
@@ -31,50 +31,56 @@ class ORB {
         this.obj = new Map<number, Stub>()
     }
     
+    //
+    // Client
+    //
+
     async connect(host: string, port: number): Promise<void> {
-        this.client = await connect("ws://"+host+":"+port)
-        console.log("_connect has returned")
+        let orb = this
+        return new Promise<void>( (resolve, reject) => {
+            orb.socket = new WebSocket("ws://"+host+":"+port)
+            orb.socket.onopen = function() {
+                resolve()
+            }
+            orb.socket.onerror = function(err) {
+                reject(err)
+            }
+        })
     }
     
     async send(data: any): Promise<any> {
         let reqid = ++this.reqid
         data.reqid = reqid
-        console.log("ORB.send(", data, ")")
 
         return new Promise<any>( (resolve, reject) => {
-            if (!this.client)
+            if (!this.socket)
                 throw Error("fuck")
-            this.client.onmessage = function(message) {
+            this.socket.onmessage = function(message) {
                 let msg = JSON.parse(message.data)
                 if (msg.glue !== "1.0")
                     throw Error("expected glue version 1.0 but got "+msg.glue)
                 if (reqid == msg.reqid)
                     resolve(msg)
             }
-            this.client.onerror = function(err) {
+            this.socket.onerror = function(err) {
                 reject(err)
             }
-            this.client.send(JSON.stringify(data))
+            this.socket.send(JSON.stringify(data))
         })
     }
 
     async create(stub: Stub, name: string): Promise<void> {
-        console.log("ORB.create(<Stub>, '"+name+"') ==> enter")
-
         let data = {
             "glue": "1.0",
-            "new": "Server",
+            "new": name,
         }
 
-        // let msg = await send(this.client, data)
         let msg = await this.send(data)
         if (msg.created === undefined) {
             throw Error("ORB.create(): message did not contain 'created'")
         }
         stub.id = msg.id
         this.obj.set(msg.id, stub)
-        console.log("stored stub for id "+msg.id)
-        console.log("ORB.create(<Stub>, '"+name+"') ==> leave")
     }
 
     async call(id: number, method: string, params: any) {
@@ -87,42 +93,39 @@ class ORB {
         return msg.result
     }
 
-    listen(host: string, port: number): void {
-        const wss = new ws.Server({host: host,port: port}, function() {
-            console.log("server ready")
-        })
-        wss.on("error", function(error) {
-            switch(error.code) {
-                case "EADDRINUSE":
-                    console.log("error: another server is already running at "+error.address+":"+error.port)
-                    break
-                default:
-                    console.log("error", error)
-            }
-        })
-        wss.on('connection', function(client) {
-            console.log("got client")
-            let orb = new ORB()
-            orb.accept(client)
+    ///
+    /// Server
+    ///
+
+    async listen(host: string, port: number): Promise<void> {
+        return new Promise<void>( (resolve, reject) => {
+            const wss = new ws.Server({host: host,port: port}, function() {
+                resolve()
+            })
+            wss.on("error", function(error) {
+                switch(error.code) {
+                    case "EADDRINUSE":
+                        reject(new Error("another server is already running at "+error.address+":"+error.port))
+                        break
+                    default:
+                        reject(error)
+                }
+            })
+            wss.on("connection", function(client) {
+                let orb = new ORB()
+                orb.accept(client)
+            })
         })
     }
     
     accept(client: ws.WebSocket) {
         let orb = this
-        client.on('open', function() {
-            console.log("open")
-        })
-        client.on('message', function(message, flags) {
+        client.on("message", function(message, flags) {
             let msg = JSON.parse(message)
-            console.log("server got message", msg)
-            for(let x of msg)
-                console.log(x)
-            
             if (msg.glue !== "1.0") {
                 throw Error("expected glue version 1.0 but got "+msg.glue)
             }
             if (msg.new !== undefined) {
-                console.log("create new object of type '"+msg.new+"'")
                 ++orb.id
                 let obj = new Server_impl(orb)
                 orb.obj.set(orb.id, obj)
@@ -132,11 +135,9 @@ class ORB {
                     "id": orb.id,
                     "reqid": msg.reqid
                 }
-                console.log("created new object, send reply ", answer)
                 client.send(JSON.stringify(answer))
             }
             if (msg.method !== undefined) {
-                console.log("server: call '"+msg.method+"' on server for object ", msg.id)
                 let stub = orb.obj.get(msg.id)
                 if (stub===undefined)
                     throw Error("ORB.accept(): client required method '"+msg.method+"' on server for unknown object "+msg.id)
@@ -236,34 +237,30 @@ if (process.argv.length!==3) {
     process.exit(1)
 }
 
+async function server() {
+    let orb = new ORB()
+    new Server_impl(orb)		// register new Server class
+    await orb.listen("0.0.0.0", 8000)
+    console.log("server ready")
+}
+
 async function client() {
     let orb = new ORB()
     await orb.connect("127.0.0.1", 8000)
     console.log("orb connected")
-console.log("----------------- create server -------------------")
     let server = new Server(orb)
     await server.create()
-console.log("----------------- invoke hello --------------------")
     server.hello()
-console.log("----------------- invoke answer --------------------")
     server.answer(2, 9)
     let n = await server.answer(7, 6)
     console.log("the answer is "+n)
 }
 
 if (process.argv[2]==="--server") {
-    let orb = new ORB()
-    orb.listen("0.0.0.0", 8000)
-    new Server_impl(orb)		// register new Server class
+    server()
 } else
 if (process.argv[2]==="--client") {
-    try {
-        client()
-    }
-    catch(error) {
-        console.log("error: "+error.message)
-        console.log(error.stack)
-    }
+    client()
 } else {
     console.log("please provide one argument: --server | --client")
     process.exit(1)
