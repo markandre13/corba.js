0e4af43d9e60b9eb28aa159553847a2416d85943
Mark-André Hopf <mhopf@mark13.org>
2021-07-18 23:06:19 +0200

    begin decoding of IOR

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = (config) => {
     },
     reporters: ["mocha", "karma-typescript"],
     karmaTypescriptConfig: {
+      stopOnFailure: false,
       tsconfig: "tsconfig.json",
       bundlerOptions: {
         sourceMap: true
