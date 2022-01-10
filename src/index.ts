828368dcc1e567fab3f578fffa79d4d6d682ce74
Mark-André Hopf <mhopf@mark13.org>
2022-01-10 22:28:49 +0100

    do include net/ directory in main export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-// export * from './orb'
-// export * from './net'
-
 export * from './orb/uint8map'
 export * from './orb/url'
 export * from './orb/asn1'
 export * from './orb/giop'
 export * from './orb/orb'
-
-export * from './net/tcp'
-export * from './net/tls'
-export * from './net/ws'
